819622f9e45714a8e3f9d530ae22d5682d58adc5
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2024-09-07 16:28:11 -0700

    fixed: leftover counter

--- scripts/fetch_figures_v2.R
+++ scripts/fetch_figures_v2.R
@@ -193,7 +193,6 @@ extract_figures <- function(xml_content, exfigids) {
   # Process each article
   cat(paste("Processing",length(processed_articles),"articles\n"))
   for (article in processed_articles) {
-    cat(paste("Processing article", j, "of", length(processed_articles),"\n"))
     journal_nlm_ta <- xml_text(xml_find_first(article, ".//journal-id[@journal-id-type='nlm-ta']"))
     publisher_name <- xml_text(xml_find_first(article, ".//publisher-name"))
     article_title <- xml_text(xml_find_first(article, ".//article-title"))
